dae73db0fa2f00195a7393037c90fa1af15e629c
易峰 <793173580@qq.com>
2019-10-27 12:10:49 +0800

    登录功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入全局样式表
+import './assets/css/global.css'
+// 引入字体图标
+import './assets/fonts/iconfont.css'
+
+// 导入element
+import './element'
+// 引入axios
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
